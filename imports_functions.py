b7154282c2e08daecd52d075ea053cfd0732985f
Domenico Guarino <domenico.guarino@gmail.com>
2022-06-22 19:48:39 +0200

    pands conflict in loading pickle; issues in the numpy version

--- imports_functions.py
+++ imports_functions.py
@@ -26,7 +26,7 @@ from functools import cmp_to_key
 from functools import reduce
 from itertools import zip_longest # analysis
 import pickle
-import pickle5 as pickle
+# import pickle5 as pickle
 
 
 import scipy.io as sio
