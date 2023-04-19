1bb3ac2daf442149140ae466ca177ccaf1f684b6
Domenico Guarino <domenico.guarino@gmail.com>
2023-04-19 17:58:29 +0200

    analysis by scan

--- imports_functions.py
+++ imports_functions.py
@@ -173,27 +173,40 @@ class OrderedCounter(Counter, OrderedDict):
 # https://github.com/AllenInstitute/MicronsBinder/blob/master/notebooks/vignette_analysis/function/lib/data.py
 # Get soma center coordinates ([um])
 def get_soma_loc(data_dict, seg_id):
-	seg_ids = data_dict["segment_id"]
-	soma_locs = data_dict["loc"]
-	return soma_locs[seg_ids==seg_id][0]
+    seg_ids = data_dict["segment_id"]
+    soma_locs = data_dict["loc"]
+    return soma_locs[seg_ids==seg_id][0]
 
 # Get calcium trace
 def get_trace(data_dict, seg_id, scan_id, trace_type="spike"):
-	seg_ids = data_dict["segment_id"]
-	scan_ids = data_dict["scan_id"]
-	traces = data_dict[trace_type]
-	valid = (seg_ids==seg_id)*(scan_ids==scan_id)
-	return traces[np.where(valid)[0][0]]
+    seg_ids = data_dict["segment_id"]
+    scan_ids = data_dict["scan_id"]
+    traces = data_dict[trace_type]
+    valid = (seg_ids==seg_id)*(scan_ids==scan_id)
+    return traces[np.where(valid)[0][0]]
 
 # Get scan id
 def get_scan(data_dict, seg_id):
-	seg_ids = data_dict["segment_id"]
-	scan_ids = data_dict["scan_id"]
-	return scan_ids[seg_ids==seg_id][0]
+    seg_ids = data_dict["segment_id"]
+    scan_ids = data_dict["scan_id"]
+    return scan_ids[seg_ids==seg_id][0]
 
 # Get synapse_conneciton density
 def get_density(data_dict, seg_id, dens_type="inconn_dens"):
-	seg_ids = data_dict["segment_id"]
-	densities = data_dict[dens_type]
-	return densities[seg_ids==seg_id][0]
+    seg_ids = data_dict["segment_id"]
+    densities = data_dict[dens_type]
+    return densities[seg_ids==seg_id][0]
+
 # ------------------------------------------------------------------------------
+
+class MidpointNormalize(mpl.colors.Normalize):
+    def __init__(self, vmin, vmax, midpoint=0, clip=False):
+        self.midpoint = midpoint
+        mpl.colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        normalized_min = max(0, 1 / 2 * (1 - abs((self.midpoint - self.vmin) / (self.midpoint - self.vmax))))
+        normalized_max = min(1, 1 / 2 * (1 + abs((self.vmax - self.midpoint) / (self.midpoint - self.vmin))))
+        normalized_mid = 0.5
+        x, y = [self.vmin, self.midpoint, self.vmax], [normalized_min, normalized_mid, normalized_max]
+        return np.ma.masked_array(np.interp(value, x, y))
